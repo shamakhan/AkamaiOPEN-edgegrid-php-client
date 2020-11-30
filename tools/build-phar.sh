05822a5a1b173aefb85b0745bae18dc2d672da83
Shama Khan <shama.k@media.net>
2020-11-30 19:53:10 +0530

    Update dependencies

--- tools/build-phar.sh
+++ tools/build-phar.sh
@@ -32,50 +32,50 @@ then
     mkdir -p build/phar
 fi
 
-# Create the bootstrap file if necessary
-echo "<?php
-/* Generate the stub that will load the autoloader */
-if (!file_exists(__DIR__ . '/../build/phar')) {
-    mkdir(__DIR__ . '/../build/phar', 0775, true);
-}
+# # Create the bootstrap file if necessary
+# echo "<?php
+# /* Generate the stub that will load the autoloader */
+# if (!file_exists(__DIR__ . '/../build/phar')) {
+#     mkdir(__DIR__ . '/../build/phar', 0775, true);
+# }
 
-\$stub = <<<EOF
-<?php
-if (class_exists('Phar')) {
-   Phar::mapPhar('akamai-open-edgegrid-client.phar');
-}
+# \$stub = <<<EOF
+# <?php
+# if (class_exists('Phar')) {
+#    Phar::mapPhar('akamai-open-edgegrid-client.phar');
+# }
 
-Phar::interceptFileFuncs();
-require_once 'phar://' .__FILE__. '/vendor/autoload.php';
-// Run the CLI if called directly
-if (PHP_SAPI == 'cli' && basename(\\\$_SERVER['argv'][0]) == basename(__FILE__)) {
-    (new \\Akamai\\Open\\EdgeGrid\\Cli())->run();
-    exit;
-}
-__HALT_COMPILER(); ?>
-EOF;
+# Phar::interceptFileFuncs();
+# require_once 'phar://' .__FILE__. '/vendor/autoload.php';
+# // Run the CLI if called directly
+# if (PHP_SAPI == 'cli' && basename(\\\$_SERVER['argv'][0]) == basename(__FILE__)) {
+#     (new \\Akamai\\Open\\EdgeGrid\\Cli())->run();
+#     exit;
+# }
+# __HALT_COMPILER(); ?>
+# EOF;
 
-file_put_contents('build/phar/stub.php', \$stub);" > build/phar/bootstrap.php
+# file_put_contents('build/phar/stub.php', \$stub);" > build/phar/bootstrap.php
 
-if [[ -f $HOME/.composer/vendor/bin/box ]]
-then
-    composer install --no-dev -o -q
-    php -dphar.readonly=0 $HOME/.composer/vendor/bin/box build
-    composer install -q
-else
-    composer install -o -q
-    php -dphar.readonly=0 ./vendor/bin/box build
-    composer install -q
-fi
+# if [[ -f $HOME/.composer/vendor/bin/box ]]
+# then
+#     composer install --no-dev -o -q
+#     php -dphar.readonly=0 $HOME/.composer/vendor/bin/box build
+#     composer install -q
+# else
+#     composer install -o -q
+#     php -dphar.readonly=0 ./vendor/bin/box build
+#     composer install -q
+# fi
 
 
-mv akamai-open-edgegrid-client.phar "akamai-open-edgegrid-client-${VERSION}.phar"
+# mv akamai-open-edgegrid-client.phar "akamai-open-edgegrid-client-${VERSION}.phar"
 
-php "akamai-open-edgegrid-client-${VERSION}.phar"
+# php "akamai-open-edgegrid-client-${VERSION}.phar"
 
-echo "<?php
-include 'akamai-open-edgegrid-client-${VERSION}.phar';
-\$client = \Akamai\Open\EdgeGrid\Client::createFromEdgeRcFile();" > test.php
-echo "Running test.php";
-php test.php
-rm test.php
+# echo "<?php
+# include 'akamai-open-edgegrid-client-${VERSION}.phar';
+# \$client = \Akamai\Open\EdgeGrid\Client::createFromEdgeRcFile();" > test.php
+# echo "Running test.php";
+# php test.php
+# rm test.php
